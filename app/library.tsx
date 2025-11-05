161e2289242d6be44a29a32f58d28d114adcbaab
ArmandBireaud <armand.bireaud@gmail.com>
2025-11-05 20:01:31 +0100

    Renaming convention

--- app/library.tsx
+++ app/library.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function LibraryScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>🏆 Library Screen</Text>
+      <Text>🏆 Library Screen </Text>
     </View>
   );
 }
